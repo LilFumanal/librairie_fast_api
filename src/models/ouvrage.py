7eb113ba433ae6df2236e04514cf7b39f41ec37d
LilFumanal <charlie.fumanal@gmail.com>
2023-11-24 12:14:49 +0100

    WIP - Routeur d'ouvrages

--- src/models/ouvrage.py
+++ src/models/ouvrage.py
@@ -1,7 +1,8 @@
 from datetime import date
 from config import Base
-from sqlalchemy import select, String
-from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column, Session
+from models import Commentaire
+from sqlalchemy import select, String, Text
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column, Session, relationship
 
 
 class Ouvrage(Base):
@@ -17,6 +18,8 @@ class Ouvrage(Base):
     date_dispo_librairie_ouvrage: Mapped[date] = mapped_column(date)
     date_dispo_particulier_ouvrage: Mapped[date] = mapped_column(date)
     #image_ouvrage: Mapped[image] = mapped_column(object)
-    table_matieres_ouvrage: Mapped[str] = mapped_column(String(255))
-    mot_clef_ouvrage: Mapped[str] = mapped_column(String(255))
-    description_ouvrage: Mapped[str] = mapped_column(String(255))
+    table_matieres_ouvrage: Mapped[Text] = mapped_column(Text)
+    mot_clef_ouvrage: Mapped[Text] = mapped_column(Text)
+    description_ouvrage: Mapped[Text] = mapped_column(Text)
+    
+    commentaires_ouvrage: Mapped[list["Commentaire"]] = relationship(back_populates = "ouvrage")
