218e5b9926eba9441f3d9071f886803c6732cdb2
Pierrick-LB <pierrick.lb2003@gmail.com>
2023-11-24 10:34:56 +0100

    modif client.py

--- src/models/client.py
+++ src/models/client.py
@@ -1,4 +1,21 @@
 from config import Base
+from typing import Optional
+from sqlalchemy import String
+from sqlalchemy.orm import Mapped, mapped_column
+
+
+
 
 class Client(Base):
-    pass
+    __tablename__ = "clients"
+    id_client: Mapped[int] = mapped_column(primary_key=True)
+    nom_client: Mapped[str] =  mapped_column(String(255))
+    prenom_client: Mapped[Optional[str]] =  mapped_column(String(255))
+    email_client: Mapped[str] = mapped_column(String(255))
+    telephone_client: Mapped[str] =  mapped_column(String(20))
+    preferences_client: Mapped[str] =  mapped_column String
+    adresse_livraison_client: Mapped[str] =  mapped_column String
+    adresse_facturation_client: Mapped[str] =  mapped_column String
+    
+    
+
