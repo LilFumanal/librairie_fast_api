0011fe65701f8cd9a326d2765c45c349d20cb028
Pierrick-LB <pierrick.lb2003@gmail.com>
2023-11-24 16:15:39 +0100

    Teste du commit

--- src/models/client.py
+++ src/models/client.py
@@ -1,19 +1,16 @@
 from config import Base
 from typing import Optional
 from sqlalchemy import String
-from sqlalchemy.orm import Mapped, mapped_column
 from sqlalchemy import Text
+from sqlalchemy.orm import Mapped, mapped_column
 
 
 
 
 class Client(Base):
-    __tablename__ = "client"
+    __tablename__ = "clients"
     id_client: Mapped[int] = mapped_column(primary_key=True)
     nom_client: Mapped[str] =  mapped_column(String(255))
     prenom_client: Mapped[Optional[str]] =  mapped_column(String(255))
     email_client: Mapped[str] = mapped_column(String(255))
     telephone_client: Mapped[str] =  mapped_column(String(20))
-    preferences_client: Mapped[Optional[str]] =  mapped_column(Text())
-    adresse_livraison_client: Mapped[Optional[str]] =  mapped_column(Text())
-    adresse_facturation_client: Mapped[Optional[str]] =  mapped_column(Text())
