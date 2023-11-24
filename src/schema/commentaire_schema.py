38d4f6310edc9e4d142e25f12fea6a30ca9faccf
FentasKodek <dekok.stef@gmail.com>
2023-11-24 17:05:07 +0100

    commentaire schema fini

--- src/schema/commentaire_schema.py
+++ src/schema/commentaire_schema.py
@@ -27,6 +27,19 @@ from datetime import date
 from pydantic import BaseModel
 
 class Commentaire(BaseModel):
+    date_publication_commentaire: date
+    commentaire: str
+    titre_commentaire: str
+
+    class Config:
+        orm_mode = True
+
+class CreateCommentaire(Commentaire):
+    id: int
+    id_client: int
+    id_ouvrage: int
+
+
 
 
 
