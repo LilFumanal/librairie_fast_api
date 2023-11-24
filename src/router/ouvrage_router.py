9cfe523cb1cf47b5e1b25a551cb1c018d29b8ac2
LilFumanal <charlie.fumanal@gmail.com>
2023-11-24 14:30:53 +0100

    Add schema et router ouvrage

--- src/router/ouvrage_router.py
+++ src/router/ouvrage_router.py
@@ -1,4 +1,23 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, FastAPI
+from models import Ouvrage
+from schema import ouvrage_schema
+from sqlalchemy.orm import Session
 
-
+app = FastAPI()
 router = APIRouter()
+
+@app.get("/ouvrage/{ouvrage.id}", response_model=ouvrage_schema.Ouvrage)
+def get_ouvrage_by_id(db: Session, ouvrage_id: int):
+  return db.query(Ouvrage).filter(Ouvrage.id_ouvrage == ouvrage_id).first()
+
+@app.get("/ouvrage/{ouvrage.auteur}", response_model=ouvrage_schema.Ouvrage)
+def get_ouvrage_by_author(db: Session, ouvrage_auteur: str):
+  return db.query(Ouvrage).filter(Ouvrage.auteur_ouvrage == ouvrage_auteur).first()
+
+@app.get("/ouvrage/{ouvrage.isbn}", response_model=ouvrage_schema.Ouvrage)
+def get_ouvrage_by_isbn(db: Session, ouvrage_isbn: str):
+  return db.query(Ouvrage).filter(Ouvrage.isbn_ouvrage == ouvrage_isbn).first()
+
+@app.get("/ouvrage", response_model=list[ouvrage_schema.Ouvrage])
+def get_ouvrages(db: Session, skip: int = 0, limit: int = 100):
+  return db.query(Ouvrage).offset(skip).limit(limit).all()
---
+++ src/schema/ouvrage_schema.py
@@ -0,0 +1,24 @@
+from datetime import date
+from pydantic import BaseModel
+from . import commentaire_schema
+
+class Ouvrage(BaseModel):
+  titre: str
+  auteur: str
+  isbn: str
+  langue: str
+  prix: float
+  date_parution: date
+  categorie: str
+  date_dispo_librairie: date
+  date_dispo_particulier: date
+  table_matieres: str
+  mot_clef: str
+  description: str
+  
+class CreateOuvrage(Ouvrage):
+  id: int
+  commentaire: list[Commentaire]
+  
+  class Config:
+    orm_mode = True
